abb0edc63ed279ec9137561bd4f04708b7fb5938
Gruppe Mac <app@gruppe.com.br>
2025-06-23 18:05:57 -0300

    producao

--- lib/widgets/button_loader_widget.dart
+++ lib/widgets/button_loader_widget.dart
@@ -10,7 +10,7 @@ class ButtonLoader extends StatefulWidget {
   final Color? background;
   final Color? borderColor;
   final double? elevation;
-
+  final double? widthBorder;
   const ButtonLoader({
     super.key,
     required this.childTitle,
@@ -21,6 +21,7 @@ class ButtonLoader extends StatefulWidget {
     this.background,
     this.borderColor,
     this.elevation = 0.0,
+    this.widthBorder = 1,
   });
   @override
   State<ButtonLoader> createState() => ButtonLoaderState();
@@ -30,6 +31,12 @@ class ButtonLoaderState extends State<ButtonLoader>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   bool _isLoading = false;
+  // @override
+  // void deactivate() {
+  //   Form.maybeOf(context)?.deactivate();
+  //   super.deactivate();
+  // }
+
   @override
   void initState() {
     super.initState();
@@ -39,6 +46,15 @@ class ButtonLoaderState extends State<ButtonLoader>
     );
   }
 
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  // void _unregister(ButtonLoaderState field) {
+  //   _fields.remove(field);
+  // }
   void startLoading() {
     if (_isLoading) return;
     setState(() {
@@ -58,12 +74,6 @@ class ButtonLoaderState extends State<ButtonLoader>
   }
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     Color? borderColor = widget.borderColor;
     double elevation = widget.elevation!;
@@ -78,16 +88,16 @@ class ButtonLoaderState extends State<ButtonLoader>
           width: widget.width ?? MediaQuery.of(context).size.width * 0.5,
           height: widget.height ?? 50,
           child: Material(
-            color: widget.background ?? Colors.white,
+            color: widget.background ?? Theme.of(context).primaryColor,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10),
               side: BorderSide(
                 color: widget.elevation == 0.0
                     ? _isLoading
                         ? widget.loaderColor.withOpacity(0)
-                        : borderColor ?? Colors.white
+                        : borderColor ?? Theme.of(context).primaryColor
                     : widget.loaderColor.withOpacity(0),
-                width: 2,
+                width: widget.widthBorder ?? 1,
               ),
             ),
             elevation: elevation,
@@ -99,11 +109,14 @@ class ButtonLoaderState extends State<ButtonLoader>
                     builder: (BuildContext context, Widget? child) {
                       return CustomPaint(
                         painter: BorderPainter(
-                          progress: _controller.value,
-                          isLoading: _isLoading,
-                          colorPaint:
-                              _isLoading ? widget.loaderColor : Colors.white,
-                        ),
+                            progress: _controller.value,
+                            isLoading: _isLoading,
+                            colorPaint: _isLoading
+                                ? widget.loaderColor
+                                : Theme.of(context).primaryColor,
+                            strokeWidth: _isLoading
+                                ? widget.widthBorder! * 2
+                                : widget.widthBorder!),
                       );
                     },
                   ),
@@ -122,16 +135,21 @@ class BorderPainter extends CustomPainter {
   final double progress;
   final bool isLoading;
   final Color colorPaint;
+  final double strokeWidth;
   BorderPainter({
     required this.progress,
     required this.isLoading,
     required this.colorPaint,
+    required this.strokeWidth,
   });
   @override
-  void paint(Canvas canvas, Size size) {
+  void paint(
+    Canvas canvas,
+    Size size,
+  ) {
     final Paint paint = Paint()
       ..color = colorPaint
-      ..strokeWidth = 4.0
+      ..strokeWidth = strokeWidth
       ..style = PaintingStyle.stroke;
     final RRect rrect = RRect.fromRectAndRadius(
       Rect.fromLTWH(0, 0, size.width, size.height),
